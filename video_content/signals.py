166248e22e41b3ed4f568a9c4f5547dadbf6f546
Tonys <info@tony-schiller.com>
2023-10-14 09:57:14 +0200

    add some video convertions

--- video_content/signals.py
+++ video_content/signals.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 
 from .models import Video
-from .tasks import convert_480p
+from .tasks import convert_video
 
 
 @receiver(post_save, sender=Video)
@@ -14,7 +14,9 @@ def video_post_save(sender, instance, created, **kwargs):
     if created:
         print('New video was created')
         queue = django_rq.get_queue('default', autocommit=True)
-        queue.enqueue(convert_480p, instance.video_file.path)
+        queue.enqueue(convert_video, instance.video_file.path, "480p")
+        queue.enqueue(convert_video, instance.video_file.path, "720p")
+        queue.enqueue(convert_video, instance.video_file.path, "1080p")
 
 
 # Help function to delete files
@@ -27,5 +29,9 @@ def _delete_file(path):
 def video_post_delete(sender, instance, **kwargs):
     if instance.video_file:
         _delete_file(instance.video_file.path)
+    if instance.video_file_480p:
         _delete_file(instance.video_file_480p.path)
-
+    if instance.video_file_720p:
+        _delete_file(instance.video_file_720p.path)
+    if instance.video_file_1080p:
+        _delete_file(instance.video_file_1080p.path)
